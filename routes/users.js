847f27a97568baaf484f93f5a7db7ff66e0f9ec2
alvin almodal <almodalalvin@gmail.com>
2021-06-14 18:07:22 +0800

    added endpoint for exercise tracker

---
+++ routes/users.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const Exercise = require("../models/exercise");
+
+const getCurrentDateTime = () => {
+  let date = new Date();
+  let year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(date);
+  let month = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(date);
+  let day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(date);
+  return `${year}-${month}-${day}`;
+};
+
+router.get("/", async (req, res) => {
+  let users = await User.find();
+  res.send(users);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { username: newUsername } = req.body;
+
+    let currentUser = await User.findOne({ username: newUsername });
+
+    if (currentUser) {
+      res.send("Username already taken");
+      return;
+    }
+
+    if (
+      newUsername &&
+      typeof newUsername === "string" &&
+      newUsername.length > 0
+    ) {
+      let { username, _id } = await User.create({ username: newUsername });
+      res.send({ username, _id });
+    } else {
+      res.status(400);
+      res.send({ error: "Username is required" });
+    }
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+router.post("/:_id/exercises", async (req, res) => {
+  try {
+    const { _id } = req.params;
+
+    let user = await User.findOne({ _id });
+
+    if (!user) {
+      res.send("not found.");
+      return;
+    }
+
+    let { description, duration, date } = req.body;
+    date = date || getCurrentDateTime();
+
+    let {
+      description: newDescription,
+      duration: newDuration,
+      date: newDate,
+      user: newUser,
+    } = await Exercise.create({
+      user: { username: user.username, _id: user._id },
+      description,
+      duration,
+      date,
+    });
+    res.send({
+      _id: newUser._id,
+      username: newUser.username,
+      description,
+      duration,
+      date,
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+router.get("/:_id/logs", async (req, res) => {
+  try {
+    let { _id } = req.params;
+    let { from, to, limit } = req.query;
+    limit = parseInt(limit) || 0;
+    let user = await User.findOne({ _id });
+
+    if (!user) {
+      res.send("Unknown userId");
+      return;
+    }
+
+    let exercises = [];
+    if (from && to) {
+      exercises = await Exercise.find({
+        "user._id": user._id,
+        date: {
+          $gte: new Date(from),
+          $lte: new Date(to),
+        },
+      }).limit(limit);
+    } else {
+      exercises = await Exercise.find({
+        "user._id": user._id,
+      }).limit(limit);
+    }
+
+    res.send({
+      _id: user._id,
+      username: user.username,
+      count: exercises.length,
+      log: exercises,
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// mongoose setup
+const mongoose = require("mongoose");
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.wqup4.mongodb.net/freecodecamp?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("mongodb connected...");
+  }
+);
+
+//routes setup
+const users = require("./routes/users");
+
+app.use("/api/users", users);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
